f89834ea3fd4f817a13f68fe54dc80050715ac6d
Alexandre Gaspar <alexandre.monteiro@serpro.gov.br>
2022-06-09 18:16:10 -0300

    Solução Ruim

--- src/application/program.java
+++ src/application/program.java
@@ -35,18 +35,16 @@ public class program {
 			System.out.print("Check-out date (dd/MM/yyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
+			String erro = reservation.updateDates(checkIn, checkOut);
 			
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates ");
+			if (erro != null) {
+				System.out.println("Error in reservation: " + erro);
 				
-			}else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out date must be after check-in date. ");
-			}else {
-				reservation.updateDates(checkIn, checkOut);
+			}else
+			{
 				System.out.println("Reservation: " + reservation);
 			}
-		
+			
 		sc.close();
 		}
 
